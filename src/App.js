7b52cb25b8f13c64a7e03e104ba9a77b26587cb7
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-08-30 16:37:02 -0300

    feat: Create global styles and css reset

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import GlobalStyles from './styles/global';
 import logo from './assets/images/code-with-beer.png';
 
 function App() {
-  return <img src={logo} />;
+  return (
+    <React.Fragment>
+      <GlobalStyles />
+      <img src={logo} />
+    </React.Fragment>
+  );
 }
 
 export default App;
