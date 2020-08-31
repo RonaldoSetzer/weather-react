95cf5de1491ba38cb57cae5a1666c3207512b793
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-08-31 15:46:37 -0300

    refactor: Rename internal color variables

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,6 +1,6 @@
 import { opacify, lighten, darken } from 'polished';
 
-export const primary = {
+export const palette = {
   gray: '#928374',
   blue: '#83a598',
   yellow: '#fabd2f',
@@ -10,31 +10,31 @@ export const primary = {
 };
 
 export const theme = {
-  colorDefault: primary.white,
-  colorInverse: primary.gray,
-  backgroundDefault: primary.dark,
-  backgroundInverse: primary.white,
+  colorDefault: palette.white,
+  colorInverse: palette.gray,
+  backgroundDefault: palette.dark,
+  backgroundInverse: palette.white,
 };
 
 export const colorlevels = {
   gray: [
-    opacify(0.7, primary.gray),
-    opacify(0.7, lighten(0.1, primary.gray)),
-    opacify(0.7, darken(0.2, primary.gray)),
+    opacify(0.7, palette.gray),
+    opacify(0.7, lighten(0.1, palette.gray)),
+    opacify(0.7, darken(0.2, palette.gray)),
   ],
   blue: [
-    opacify(0.7, primary.blue),
-    opacify(0.7, lighten(0.1, primary.blue)),
-    opacify(0.7, darken(0.2, primary.blue)),
+    opacify(0.7, palette.blue),
+    opacify(0.7, lighten(0.1, palette.blue)),
+    opacify(0.7, darken(0.2, palette.blue)),
   ],
   yellow: [
-    opacify(0.7, primary.yellow),
-    opacify(0.7, lighten(0.1, primary.yellow)),
-    opacify(0.7, darken(0.2, primary.yellow)),
+    opacify(0.7, palette.yellow),
+    opacify(0.7, lighten(0.1, palette.yellow)),
+    opacify(0.7, darken(0.2, palette.yellow)),
   ],
   red: [
-    opacify(0.7, primary.red),
-    opacify(0.7, lighten(0.1, primary.red)),
-    opacify(0.7, darken(0.2, primary.red)),
+    opacify(0.7, palette.red),
+    opacify(0.7, lighten(0.1, palette.red)),
+    opacify(0.7, darken(0.2, palette.red)),
   ],
 };
