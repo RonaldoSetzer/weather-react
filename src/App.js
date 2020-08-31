b57a3252268aad4fad4d5e4633de713a13204664
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-08-31 19:12:23 -0300

    feat: Setup Redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
 import GlobalStyles from './styles/global';
 import Forecast from './pages/Forecast';
 import PhotoBackground from './components/PhotoBackground';
 
+import store from './store';
+
 function App() {
   return (
-    <React.Fragment>
+    <Provider store={store}>
       <GlobalStyles />
       <PhotoBackground>
         <Forecast />
       </PhotoBackground>
-    </React.Fragment>
+    </Provider>
   );
 }
 
---
+++ src/store/modules/rootReducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+function reduxExemple(state = [], action) {
+  return state;
+}
+
+export default combineReducers({ reduxExemple });
