d4170fbc28d9010a3285c796c55fc044fed2a72c
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-08-31 15:29:26 -0300

    refactor: Retrieve colors from color utils

--- src/assets/icons/Compass.js
+++ src/assets/icons/Compass.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { theme } from '../../utils/colors';
 
-function Compass({ fill = '#fff' }) {
+function Compass({ fill = theme.colorInverse }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="100"
-      height="100"
+      width="32"
+      height="32"
       x="0"
       y="0"
       version="1.1"
