21cfa8720ca0ef3f152e28cb0765c83e70240f5a
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-08-31 15:47:23 -0300

    doc: Update Design System colors on storybook

--- src/stories/Colors.stories.js
+++ src/stories/Colors.stories.js
@@ -1,15 +1,78 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { colorlevels, palette } from '../utils/colors';
+
+const Container = styled.div`
+  span {
+    width: 5rem;
+    text-transform: uppercase;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  li {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const List = styled.div`
+  list-style: none;
+  display: flex;
+  align-items: center;
+`;
+
 const ColorBox = styled.div`
   width: 3rem;
   height: 3rem;
   background: ${props => props.color};
 `;
 
+export const Palette = () => (
+  <Container>
+    <ul>
+      {Object.keys(palette).map(key => (
+        <li>
+          <span>{key}</span>
+          <ColorBox color={palette[key]} />
+        </li>
+      ))}
+    </ul>
+  </Container>
+);
+
+export const WeatherLevels = () => (
+  <Container>
+    <List>
+      <span>gray</span>
+      {colorlevels.gray.map(color => (
+        <ColorBox color={color} />
+      ))}
+    </List>
+    <List>
+      <span>Blue</span>
+      {colorlevels.blue.map(color => (
+        <ColorBox color={color} />
+      ))}
+    </List>
+    <List>
+      <span>yellow</span>
+      {colorlevels.yellow.map(color => (
+        <ColorBox color={color} />
+      ))}
+    </List>
+    <List>
+      <span>red</span>
+      {colorlevels.red.map(color => (
+        <ColorBox color={color} />
+      ))}
+    </List>
+  </Container>
+);
+
 export default {
   title: 'Design System/Colors',
-  component: ColorBox,
 };
-
-export const Yellow = () => <ColorBox color="#ffff00" />;
