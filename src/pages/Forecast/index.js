071523d6e6ec9853d5282c0dfd7ec82561f749b8
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-08-31 17:54:42 -0300

    feat: Implement the right layout to Forecast page

--- src/pages/Forecast/index.js
+++ src/pages/Forecast/index.js
@@ -1,14 +1,72 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-import PhotoBackground from '../../components/PhotoBackground';
+import SearchBox from '../../components/SearchBox';
+import WeatherCard from '../../components/WeatherCard';
+import WeatherInfo from '../../components/WeatherInfo';
+
+import { ClearDay } from '../../assets/icons';
+import { colorlevels } from '../../utils/colors';
+import { Title, List } from '../../components/ui';
+
+import { Container } from './styles';
+
+const [normal, lighten, darken] = colorlevels.yellow;
+
+const weather = {
+  today: {
+    label: 'Today',
+    icon: <ClearDay />,
+    temperature: '32C',
+    backgroundColor: normal,
+    info: {
+      type: 'Sunny',
+      wind: 'NO 6.4km/h',
+      humidity: '78%',
+      pressure: '1003hPA',
+    },
+  },
+  tomorrow: {
+    label: 'Tomorrow',
+    temperature: '30C',
+    backgroundColor: lighten,
+  },
+  afterTomorrow: {
+    label: 'After Tomorrow',
+    temperature: '30C',
+    backgroundColor: darken,
+  },
+};
 
 function Forecast() {
+  const { today, tomorrow, afterTomorrow } = weather;
+
   return (
-    <div>
-      Forecast
-      <PhotoBackground />
-    </div>
+    <Container>
+      <SearchBox />
+      <WeatherCard
+        label={today.label}
+        icon={today.icon}
+        temperature={today.temperature}
+        backgroundColor={today.backgroundColor}
+      >
+        <WeatherInfo
+          title={today.info.type}
+          wind={today.info.wind}
+          humidity={today.info.humidity}
+          pressure={today.info.pressure}
+        />
+      </WeatherCard>
+      <WeatherCard
+        label={tomorrow.label}
+        temperature={tomorrow.temperature}
+        backgroundColor={tomorrow.backgroundColor}
+      />
+      <WeatherCard
+        label={afterTomorrow.label}
+        temperature={afterTomorrow.temperature}
+        backgroundColor={afterTomorrow.backgroundColor}
+      />
+    </Container>
   );
 }
 
