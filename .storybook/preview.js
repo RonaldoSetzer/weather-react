b7fe53214386ef59f074173291c6c6bfde290871
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-08-31 00:08:29 -0300

    chore: Setup GlobalStyles as Story decorators

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
+import React from 'react';
+import GlobalStyles from '../src/styles/global';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
+
+export const decorators = [
+  Story => (
+    <>
+      <GlobalStyles />
+      <Story />
+    </>
+  ),
+];
