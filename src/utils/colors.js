bebfca9f5983db3985b299858d6aff757ff0cc6a
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-08-31 03:51:43 -0300

    feat: Create color utils

---
+++ src/utils/colors.js
@@ -0,0 +1,31 @@
+import { opacify, lighten, darken } from 'polished';
+
+export const primary = {
+  gray: '#928374',
+  blue: '#83a598',
+  yellow: '#fabd2f',
+  red: '#fb4934',
+};
+
+export const colorlevels = {
+  gray: [
+    opacify(0.7, primary.gray),
+    opacify(0.7, lighten(0.1, primary.gray)),
+    opacify(0.7, darken(0.2, primary.gray)),
+  ],
+  blue: [
+    opacify(0.7, primary.blue),
+    opacify(0.7, lighten(0.1, primary.blue)),
+    opacify(0.7, darken(0.2, primary.blue)),
+  ],
+  yellow: [
+    opacify(0.7, primary.yellow),
+    opacify(0.7, lighten(0.1, primary.yellow)),
+    opacify(0.7, darken(0.2, primary.yellow)),
+  ],
+  red: [
+    opacify(0.7, primary.red),
+    opacify(0.7, lighten(0.1, primary.red)),
+    opacify(0.7, darken(0.2, primary.red)),
+  ],
+};
