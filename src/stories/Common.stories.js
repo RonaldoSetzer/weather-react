db46a215a4ea9e6f1235b99907a6e1093e84a9a1
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-08-31 16:49:29 -0300

    doc: Add common ui components on Storybooks

---
+++ src/stories/Common.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Title, List } from '../components/ui';
+
+export const title = () => <Title>Title</Title>;
+
+export const titleWithUppercase = () => <Title uppercase>Title</Title>;
+
+export const list = () => (
+  <>
+    <h1>List</h1>
+    <List>
+      <li>list doesn't have styles</li>
+      <li>list doesn't almost a minimun of line-height</li>
+      <li>list has padding-top of 0.5rem</li>
+      <li>list has padding-bottom of 0.5rem</li>
+    </List>
+  </>
+);
+
+export default {
+  title: 'Design System/Common',
+};
