08450442d50eac964706aaffd72f7446cf304de5
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-09-01 18:24:08 -0300

    feat: Implement wind format

--- src/helpers/weather.js
+++ src/helpers/weather.js
@@ -1,5 +1,25 @@
+const compass = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW', 'N'];
+// const compass = ['N', 'E', 'S', 'W', 'N'];
+
+export function convertMsToKmH(value) {
+  return Number(parseFloat(value) * 18.5).toFixed(1);
+}
+
+export function formatWindDirection(deg) {
+  const sliceSize = 360 / (compass.length - 1);
+  const mod = deg % 360;
+
+  let index = Math.round(mod / sliceSize);
+  return compass[index];
+}
+
 export function formatWind({ speed, deg }) {
-  return speed ? `${speed}m/s` : '--';
+  if (speed) {
+    const direction = formatWindDirection(deg);
+    const velocity = convertMsToKmH(speed);
+    return `${direction} ${velocity}Km/h`;
+  }
+  return '--';
 }
 
 export function formatPressure(value) {
