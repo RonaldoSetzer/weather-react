6cb36e181274a39857ab3867734ac18e9b732ff0
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-09-01 06:05:38 -0300

    feat: Create mapTemperature helper

---
+++ src/helpers/mapTemperature.js
@@ -0,0 +1,37 @@
+import { colorlevels } from '../utils/colors';
+
+export function convertToFahrenheit(celsius) {
+  let fahrenheit = (celsius * 9) / 5 + 32;
+
+  return fahrenheit;
+}
+
+export function formatCelsiusText(celsius) {
+  return celsius ? `${celsius} ˚C` : '--';
+}
+
+export function formatFahrenheitText(celsius) {
+  return celsius ? `${convertToFahrenheit(celsius)} F` : '--';
+}
+
+export function handleBackgroundColor(celsius, forecastIndex) {
+  if (celsius < 15) {
+    return colorlevels.blue[forecastIndex || 0];
+  } else if (celsius <= 35) {
+    return colorlevels.yellow[forecastIndex || 0];
+  } else if (celsius > 35) {
+    return colorlevels.red[forecastIndex || 0];
+  }
+
+  return colorlevels.gray[forecastIndex || 0];
+}
+
+export default function mapTemperature(celsius, forecastIndex) {
+  const backgroundColor = handleBackgroundColor(celsius, forecastIndex);
+
+  return {
+    celsius: formatCelsiusText(celsius),
+    fahrenheit: formatFahrenheitText(celsius),
+    backgroundColor,
+  };
+}
