ebb765a73def337c39a68522699259fdeda029db
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-09-01 03:07:17 -0300

    feat: Setup reactotron to support saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,15 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
-import rootReducer from './modules/rootReducer';
-import rootSaga from './modules/rootSaga';
+import rootReducer from '../store/modules/rootReducer';
+import rootSaga from '../store/modules/rootSaga';
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMonitor =
+  process.env.NODE_ENV === 'development'
+    ? console.tron.createSagaMonitor()
+    : null;
+
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 const enhancer =
   process.env.NODE_ENV === 'development'
