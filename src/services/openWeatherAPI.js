d2da4c2bc60d05170b89afbb01a148796348003f
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-09-01 16:21:49 -0300

    feat: Add support to getWeather on openWeatherAPI

--- src/services/openWeatherAPI.js
+++ src/services/openWeatherAPI.js
@@ -4,11 +4,16 @@ import { mapTemperature } from '../helpers/temperature';
 
 const OPEN_WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5';
 const OPEN_WEATHER_KEY = '7ba73e0eb8efe773ed08bfd0627f07b8';
-const OPEN_WEATHER_API = `/forecast`;
+const OPEN_WEATHER_FORECAST_API = `/forecast`;
+const OPEN_WEATHER_WEATHER_API = `/weather`;
 
 export function getForecast(city) {
   const params = `?q=${city}&APPID=${OPEN_WEATHER_KEY}&cnt=3&units=metric`;
-  const url = ''.concat(OPEN_WEATHER_API_URL, OPEN_WEATHER_API, params);
+  const url = ''.concat(
+    OPEN_WEATHER_API_URL,
+    OPEN_WEATHER_FORECAST_API,
+    params,
+  );
 
   return axios.get(url).then(({ data }) => {
     if (data.list) {
@@ -22,5 +27,15 @@ export function getForecast(city) {
 
       return { today, tomorrow, afterTomorrow };
     }
+  });
+}
+
+export function getWeather(city) {
+  const params = `?q=${city}&APPID=${OPEN_WEATHER_KEY}`;
+  const url = ''.concat(OPEN_WEATHER_API_URL, OPEN_WEATHER_WEATHER_API, params);
+
+  return axios.get(url).then(({ data }) => {
+    console.log(data)
+    return data
   });
 }
