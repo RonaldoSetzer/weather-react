9f7c79a08a6f6992ca9db09738e52e4518e24bd5
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-09-01 06:06:19 -0300

    feat: Add initalValues to forecast in weather reducer

--- src/store/modules/weather/reducer.js
+++ src/store/modules/weather/reducer.js
@@ -1,9 +1,12 @@
+import mapTemperature from '../../../helpers/mapTemperature';
+
 const initialState = {
   weather: null,
-  today: null,
-  tomorrow: null,
-  afterTomorrow: null,
+  today: mapTemperature(null, 0),
+  tomorrow: mapTemperature(null, 1),
+  afterTomorrow: mapTemperature(null, 2),
 };
+console.log(initialState)
 
 function weather(state = initialState, action) {
   switch (action.type) {
