9f9e7fc9ea59834a54fde76114e4f87a8f9d0a5a
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-09-02 19:15:03 -0300

    clean: Remove background store/reducer/actions/sagas

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 
-import background from './background/reducer';
 import forecast from './forecast/reducer';
 import weather from './weather/reducer';
 
-export default combineReducers({ background, forecast, weather });
+export default combineReducers({ forecast, weather });
--- src/store/modules/rootSaga.js
+++ src/store/modules/rootSaga.js
@@ -1,9 +1,8 @@
 import { all } from 'redux-saga/effects';
 
-import background from './background/sagas';
 import forecast from './forecast/sagas';
 import weather from './weather/sagas';
 
 export default function* rootSaga() {
-  return yield all([background, forecast, weather]);
+  return yield all([forecast, weather]);
 }
