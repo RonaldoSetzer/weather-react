b8427ce90e0d64aa2480f302a738113d12eeca4d
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-09-04 00:40:49 -0300

    style: Update background style height

--- src/components/PhotoBackground/styles.js
+++ src/components/PhotoBackground/styles.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 export const Container = styled.div`
   ${props => props.background && `background: url(${props.background});`}
   width: 100vw;
-  height: 100vh;
+  height: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
 `;
